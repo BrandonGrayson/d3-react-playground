e71be834b6541bb389eb75319d96bdb3f7642890
Brandon <brandonbgrayson@gmail.com>
2022-03-30 13:07:05 -0400

    csv data url and data property set to null

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import './App.css';
+import { useState } from 'react';
+
+const width = 960
+const height = 500
+
+const csvUrl = 'https://gist.githubusercontent.com/curran/0ac4077c7fc6390f5dd33bf5c06cb5ff/raw/605c54080c7a93a417a3cea93fd52e7550e76500/UN_Population_2019.csv'
 
 function App() {
+  const [data, setData] = useState(null)
+
   return (
     <div className="App">
       <h1>D3 Playground</h1>
