b03950b0750f58bd7dd3a9c6c9d2b650d11e06ea
Brandon <brandonbgrayson@gmail.com>
2022-04-25 12:17:37 -0400

    wrapper ref and dimensions

--- src/MedTimeline.js
+++ src/MedTimeline.js
@@ -1,5 +1,4 @@
 import React, {
-    useState,
     useEffect,
     useRef
 } from 'react';
@@ -9,10 +8,23 @@ import useResizeObserver from './useResizeObserver';
 
 export default function MedTimeline() {
     const svgRef = useRef(testData.cm)
+    const wrapperRef = useRef()
+    const dimensions = useResizeObserver(wrapperRef)
+
+
+    useEffect(() => {
+        if (!dimensions) return;
+       const svg = d3.select(svgRef.current)
+
+
+    })
 
     return (
+        <div ref={wrapperRef}>
         <svg ref={svgRef}>
-
+            <g className="x-axis" />
         </svg>
+        </div>
+
     )
 }
