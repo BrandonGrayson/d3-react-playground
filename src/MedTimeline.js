e1d9860251a636b9bce7612178d551e3bfc5871a
Brandon Grayson <brandon.grayson@pihealth.ai>
2022-04-25 16:32:27 -0400

    svg width with xAxis

--- src/MedTimeline.js
+++ src/MedTimeline.js
@@ -7,7 +7,8 @@ import * as d3 from 'd3';
 import testData from './testData';
 import useResizeObserver from './useResizeObserver';
 
-
+const svgWidth = 1600
+const svgHeight = 600
 
 const getDate = dateString => {
     const date = dateString.split("-");
@@ -17,8 +18,8 @@ const getDate = dateString => {
 export default function MedTimeline() {
     const [data, setData] = useState(testData.cm)
     const svgRef = useRef()
-    const wrapperRef = useRef()
-    const dimensions = useResizeObserver(wrapperRef)
+    // const wrapperRef = useRef()
+    // const dimensions = useResizeObserver(wrapperRef)
 
     const minDate = d3.min(data, dose => {
         return getDate(dose.rx_start_date)
@@ -29,26 +30,33 @@ export default function MedTimeline() {
     })
 
     useEffect(() => {
-        if (!dimensions) return;
+    
        const svg = d3.select(svgRef.current)
 
-       const xScale = d3.scaleTime().domain([minDate, maxDate]).range([0, dimensions.width])
+       const xScale = d3.scaleTime().domain([minDate, maxDate]).range([0, svgWidth])
 
        const xAxis = d3.axisBottom(xScale)
 
         svg.select(".x-axis").style("transform", `translateY(${450}px)`).call(xAxis)
 
 
+        // const YScale = d3.scaleLinear().domain([0, testData.length]).range([0, svgHeight])
+
+        // const yAxis = d3.axisLeft(YScale)
+
+        // svg.select(".y-axis").call(yAxis)
 
 
-    }, [dimensions])
+
+    }, [])
 
     return (
-        <div ref={wrapperRef}>
-        <svg ref={svgRef}>
+        
+        <svg ref={svgRef} width={svgWidth} height={svgHeight}>
             <g className="x-axis" />
+            <g className="y-axis" />
         </svg>
-        </div>
+       
 
     )
 }
